8db88ff3a52d3453de56c982c4c9c4c23451b98f
Vansh kumar <144512033+vVanshkumar@users.noreply.github.com>
2024-09-20 01:19:55 +0530

    Add files via upload

---
+++ Cohort Python Day 1.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 'chhayank'
+
+
+# In[2]:
+
+
+ab = 75
+
+
+# In[3]:
+
+
+x
+
+
+# In[4]:
+
+
+x = "Chhayank"
+
+
+# In[5]:
+
+
+pip install random
+
+
+# In[6]:
+
+
+pip install os
+
+
+# In[7]:
+
+
+import os
+
+
+# In[8]:
+
+
+print ("abcd")
+
+
+# In[9]:
+
+
+print (x)
+
+
+# In[10]:
+
+
+print ("name:", x)
+
+
+# In[11]:
+
+
+number1 = 24
+
+
+# In[12]:
+
+
+number2 = 444
+
+
+# In[13]:
+
+
+result = number1 + number2
+
+
+# In[14]:
+
+
+result
+
+
+# In[15]:
+
+
+print("The sum of number1 and number2 is:",result)
+
